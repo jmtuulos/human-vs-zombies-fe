592b6107b28e28428706522b2de317ab34a6e51f
Juha <viderus@gmail.com>
2023-03-09 19:08:45 +0200

    fix: altered navbar to be more presentable

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,28 +5,30 @@ const Navbar = () => {
   const { user } = useUser()
 
   return (
-    <nav class="navbar navbar-expand-sm navbar-light">
-        <a class="navbar-brand" href="/">HvZ</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <li class="nav-link active" aria-current="page"><NavLink to='/'></NavLink><span class="visually-hidden">(current)</span></li>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/gamedetails">Game Details</a>
-                </li>
-            </ul>
-            <form class="d-flex my-2 my-lg-0">
-                <input class="form-control me-sm-2" type="text" placeholder="Search"/>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
+    <div className="container pt-7">
+      <nav class="navbar navbar-expand-sm navbar-light">
+          <p class="navbar-brand">HvZ</p>
+          <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation"></button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                  <li class="nav-item">
+                      <li class="nav-link active" aria-current="page"><NavLink to='/'></NavLink><span class="visually-hidden">(current)</span></li>
+                  </li>
+                  <li class="nav-item">
+                    <NavLink to='/login'><li class="nav-link">login</li></NavLink>
+                  </li>
+                  <li class="nav-item">
+                      <li class="nav-link"><NavLink to='/gamedetails'>Game Details</NavLink></li>
+                  </li>
+              </ul>
+              <form class="d-flex my-2 my-lg-0">
+                  <input class="form-control me-sm-2" type="text" placeholder="Search"/>
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+          </div>
+      </nav>
+    </div>
   )
 }
 
