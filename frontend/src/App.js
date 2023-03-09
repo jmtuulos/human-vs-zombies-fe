a5bf467c13e20bab004c4f5b8def0712bb681286
Juha <viderus@gmail.com>
2023-03-09 19:09:41 +0200

    Added roboto text package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LandingPage from './views/LandingPage';
 import GameDetails from './views/GameDetails';
