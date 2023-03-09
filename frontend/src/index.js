27807c2ee15f79e77ca0188a802265aee64d036e
samuelahw <samuel.ahjoniemi@gmail.com>
2023-03-09 16:02:11 +0200

    add keycloak authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { initialize } from "./keycloak";
+import Loading from "./components/loading/Loading";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Display a loading screen when connecting to Keycloak
+root.render(<Loading message="Connecting to Keycloak..." />)
+
+// Initialize Keycloak
+initialize()
+  .then(() => { // If No Keycloak Error occurred - Display the App
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch(() => {
+    root.render(
+      <React.StrictMode>
+        <p>Could Not Connect To Keycloak.</p>
+      </React.StrictMode>
+    );
+  });
