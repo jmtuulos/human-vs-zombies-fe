4479a81f513691276ac917583deb5625c4bd2e37
Juha <viderus@gmail.com>
2023-03-23 12:27:36 +0200

    added markers skeleton

--- frontend/src/components/Gamedetails/Map.jsx
+++ frontend/src/components/Gamedetails/Map.jsx
@@ -1,6 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet'
+import { getAllBites } from '../../api/bite'
+import { storageRead } from '../../utils/storage'
 
 const Map = ({ coordinates }) => {
+  const gameId = storageRead('gameId')
+
+  const { isError, isLoading, data, error } = useQuery(
+    { queryKey: ['title', gameId],
+    queryFn: () => getAllBites(gameId),
+    staleTime: 10000
+  })
 
   //transform coordinates to a list form
   const latlngs = coordinates.map((coordinate) => [coordinate.longitude, coordinate.latitude])
@@ -11,11 +21,11 @@ const Map = ({ coordinates }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* {coordinates.map((e, i) => <Marker key={i} position={[e.latitude, e.longitude]}>
+      {data && data.map((e, i) => <Marker key={i} position={[e.latitude, e.longitude]}>
         <Popup>
           Marker
         </Popup>
-      </Marker>)} */}
+      </Marker>)}
       <Polygon positions={latlngs}></Polygon>
 
     </MapContainer>
