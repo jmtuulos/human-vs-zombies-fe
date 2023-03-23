044a1993168eb29b23e166ee43e682a622595233
Juha <viderus@gmail.com>
2023-03-23 21:11:44 +0200

    fix: longitude and latitude were flipped

--- frontend/src/components/Gamedetails/GameDetail.jsx
+++ frontend/src/components/Gamedetails/GameDetail.jsx
@@ -1,20 +1,23 @@
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { getAllBites } from "../../api/bite";
 import { getGame } from "../../api/game";
+import { getSquadCheckIns } from "../../api/squad";
+import { useUser } from "../../context/UserContext";
 import { storageRead } from "../../utils/storage";
 import Map from "./Map";
 
 export const GameDetail = () => {
 
   const gameId = storageRead("gameId")
+  const { user } = useUser()
 
-  const [game, bites] = useQueries({
+  const [game, bites, checkIns] = useQueries({
     queries: [
       { queryKey: ['getgame', gameId], queryFn: () => getGame(gameId), staleTime: 10000},
-      { queryKey: ['getbites', gameId], queryFn: () => getAllBites(gameId), staleTime: 10000}
-    ]
+      { queryKey: ['getbites', gameId], queryFn: () => getAllBites(gameId), staleTime: 10000},
+      { queryKey: ['getcheckins'], queryFn: () => getSquadCheckIns(user.squadId), staleTime: 10000}
+    ],
   })
-
   return (
   <div className="row border">
     {game.data &&
@@ -27,7 +30,11 @@ export const GameDetail = () => {
         </div>
       </div>
       <div className="row pt-5 h-50 w-100 d-inline-block">
-        <Map coordinates={game.data.mapCoordinates} bites={bites.data} />
+        <Map
+        coordinates={game.data.mapCoordinates}
+        bites={bites.data}
+        checkins={checkIns.data}
+        />
       </div>
     </>
     }
--- frontend/src/components/Gamedetails/Map.jsx
+++ frontend/src/components/Gamedetails/Map.jsx
@@ -1,10 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet'
 import { iconGraveStone } from '../../icons/gravestoneIcon'
 import L from 'leaflet'
+import { iconPlayer } from '../../icons/playericon'
+import { useUser } from '../../context/UserContext'
+import { iconZombie } from '../../icons/zombieicon'
 
-const Map = ({coordinates, bites}) => {
-  const latlngs = coordinates.map((coordinate) => [coordinate.longitude, coordinate.latitude])
-
+const Map = ({coordinates, bites, checkins}) => {
+  const latlngs = coordinates.map((coordinate) => [coordinate.latitude, coordinate.longitude])
+  const { user } = useUser()
+  const playerIcon = user.isHuman ? iconPlayer : iconZombie
 
   return (
     <MapContainer center={L.latLngBounds(latlngs).getCenter()} zoom={15} scrollWheelZoom={false}>
@@ -13,14 +17,24 @@ const Map = ({coordinates, bites}) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {bites && bites.map((e, i) =>
-      {
-        return( <Marker icon={ iconGraveStone } key={i} position={[e.longitude, e.latitude]}>
+       <Marker icon={ iconGraveStone }
+          key={i}
+          position={[e.latitude, e.longitude]}>
         <Popup>
           <h6>description: {e.story}</h6>
           <p>Victim id: {e.victimId}</p>
           <p>Killed id: {e.biterId}</p>
         </Popup>
-      </Marker>)})}
+        </Marker>)}
+        {checkins && checkins.map((e, i) =>
+          <Marker icon={ playerIcon }
+          key={i}
+          position={[e.latitude, e.longitude]}
+        >
+        <Popup>
+          <h6>Checked in {e.story}</h6>
+        </Popup>
+      </Marker>)}
       <Polygon positions={latlngs}></Polygon>
 
     </MapContainer>
