2e80b8b739e262cc321f711901dfa74a17d6f8b7
Juha <viderus@gmail.com>
2023-03-23 12:58:37 +0200

    fix: reverted back the query from map component

--- frontend/src/components/Gamedetails/Map.jsx
+++ frontend/src/components/Gamedetails/Map.jsx
@@ -1,19 +1,52 @@
-import { useQuery } from '@tanstack/react-query'
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet'
-import { getAllBites } from '../../api/bite'
-import { storageRead } from '../../utils/storage'
 
-const Map = ({ coordinates }) => {
-  const gameId = storageRead('gameId')
+const Map = ({coordinates}) => {
 
-  const { isError, isLoading, data, error } = useQuery(
-    { queryKey: ['title', gameId],
-    queryFn: () => getAllBites(gameId),
-    staleTime: 10000
-  })
 
-  //transform coordinates to a list form
+
+  // const coordinates =
+  console.log(coordinates)
+
+  // const coordinates = [{ long: "24.9386644", lat: "60.169909" },
+  // { long: "24.9423552", lat: "60.1675821" },
+  // { long: "24.9568391", lat: "60.1679236" },
+  // { long: "24.9563241", lat: "60.1740395" },
+  // { long: "24.9504018", lat: "60.1740181" },
+  // { long: "24.9446726", lat: "60.1703573" },
+  // { long: "24.9497366", lat: "60.1706882" }
+  // ]
+
   const latlngs = coordinates.map((coordinate) => [coordinate.longitude, coordinate.latitude])
+  // const latlngs = [
+  //   [
+  //     60.169909,
+  //     24.9386644
+  //   ],
+  //   [
+  //     60.1675821,
+  //     24.9423552
+  //   ],
+  //   [
+  //     60.1679236,
+  //     24.9568391
+  //   ],
+  //   [
+  //     60.1740395,
+  //     24.9563241
+  //   ],
+  //   [
+  //     60.1740181,
+  //     24.9504018
+  //   ],
+  //   [
+  //     60.1703573,
+  //     24.9446726
+  //   ],
+  //   [
+  //     60.1699303,
+  //     24.9386859
+  //   ]
+  // ];
 
   return (
     <MapContainer center={L.latLngBounds(latlngs).getCenter()} zoom={15} scrollWheelZoom={false}>
@@ -21,11 +54,11 @@ const Map = ({ coordinates }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {data && data.map((e, i) => <Marker key={i} position={[e.latitude, e.longitude]}>
+      {/* {coordinates.map((e, i) => <Marker key={i} position={[e.latitude, e.longitude]}>
         <Popup>
           Marker
         </Popup>
-      </Marker>)}
+      </Marker>)} */}
       <Polygon positions={latlngs}></Polygon>
 
     </MapContainer>
