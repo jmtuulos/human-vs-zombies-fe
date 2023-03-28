0db3d1eb2f667cbf34b5b5501a79093cdf14ee51
jetrosa <jetro.saarti@gmail.com>
2023-03-28 21:34:38 +0300

    Fix game player count (without auth)

--- frontend/src/views/LandingPage.jsx
+++ frontend/src/views/LandingPage.jsx
@@ -34,7 +34,7 @@ const LandingPage = () => {
             {data.map((e) =>
               <li key={e.id} className="list-group-item" style={{ backgroundColor: '#e9e3d6a3' }}>
                 <Button onClick={() => handleSelectClick(e)} variant="text" style={{ color: '#524e45' }}>
-                  {e.name}  &emsp; &#x25cf; &emsp;  {e.gameState} &emsp; &#x25cf; &emsp; players: <NumberOfPlayers id={e.id} /> &emsp; &#x25cf; &emsp; started: {new Date(e.startDateTime).toString().slice(0, 21)} &emsp; &#x25cf; &emsp; {e.description}</Button>
+                  {e.name}  &emsp; &#x25cf; &emsp;  {e.gameState} &emsp; &#x25cf; &emsp; players: {e.playerCount} &emsp; &#x25cf; &emsp; started: {new Date(e.startDateTime).toString().slice(0, 21)} &emsp; &#x25cf; &emsp; {e.description}</Button>
               </li>)
             }
           </ul>
