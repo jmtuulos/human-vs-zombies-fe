d71d3d16d1a699798eae99a9d6c82abba47485de
Juha <viderus@gmail.com>
2023-03-31 14:11:43 +0300

    fix: added how to play the game to guides

--- frontend/src/views/Guides.jsx
+++ frontend/src/views/Guides.jsx
@@ -4,7 +4,7 @@ const Guides = () => {
   return(
     <Grid className="p-5">
       <iframe style={{paddingRight: '1rem'}} src="https://scribehow.com/embed/How_to_Create_a_New_Game_in_Admin_Dashboard__yzBIhofGSpKw7MHEyIwNbA" width="640" height="640" allowfullscreen frameborder="0"></iframe>
-      <iframe style={{paddingRight: '1rem'}} src="https://scribehow.com/embed/How_to_Login_and_Join_a_Game_on_Human_vs_Zombies__95MbfLx-RKaisx0aT0vF-w" width="640" height="640" allowfullscreen frameborder="0"></iframe>
+      <iframe  style={{paddingRight: '1rem'}} src="https://scribehow.com/embed/Workflow__dtvJ627eS02wfd_WoRgpZQ" width="640" height="640" allowfullscreen frameborder="0"></iframe>
     </Grid>
   )
 }
